945d3ec65f731806301f2b0baf081cbc7e6e88c5
Noushin <nou.babakhani@gmail.com>
2023-09-13 23:11:25 +0100

    LongPress file added

--- src/test/java/noushinTest/LongPress.java
+++ src/test/java/noushinTest/LongPress.java
@@ -2,28 +2,29 @@ package noushinTest;
 
 import java.net.MalformedURLException;
 
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+import org.openqa.selenium.remote.RemoteWebElement;
+
 import io.appium.java_client.AppiumBy;
 
 public class LongPress extends BaseTest {
 
 	@Test
-	public void appiumTest() throws MalformedURLException {
-		// code to start server
-		// AndroidDriver
-		// Appium Code -> Appium Server -> mobile
-        // actual automation
-		//Xpath, ID ,AccessabilityID
-		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
-		driver.findElement(AppiumBy.id("android:id/checkbox")).click();
-		driver.findElement(AppiumBy.xpath("//android.widget.LinearLayout[2]")).click();
-		//driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.RelativeLayout")).click();
-		String alertTitle=driver.findElement(AppiumBy.id("android:id/alertTitle")).getText();
-		Assert.assertEquals(alertTitle, "WiFi settings");
-		driver.findElement(AppiumBy.id("android:id/edit")).sendKeys("Noushin");
-		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
+	public void LongPressGesture() throws MalformedURLException {
+		
+		
+		driver.findElement(AppiumBy.accessibilityId("Views")).click();
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Expandable Lists\"]")).click();
+		driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
+		WebElement ele=driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='People Names']"));
+		 ((JavsscriptExecutor)driver).executeScript("mobile: longClickGesture",
+				 ImmutableMap.of("elementId",((RemoteWebElement)ele).getId(),
+						 "duration",2000));
 	}
 }
