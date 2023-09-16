af9372a0e1ce6a39b1d7d708b9e1af42bffd5741
Noushin <nou.babakhani@gmail.com>
2023-09-16 12:09:28 +0100

    new application added

--- src/test/java/noushinTest/MiscellaneousAppium.java
+++ src/test/java/noushinTest/MiscellaneousAppium.java
@@ -7,25 +7,41 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
 public class MiscellaneousAppium extends BaseTest {
 	@Test
-	public void Miscellaneous() throws MalformedURLException {
-	
-		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]"))
-				.click();
+	public void Miscellaneous() throws MalformedURLException, InterruptedException {
+		// App Package & App Activity
+		//adb shell dumpsys window | find "mCurrentFocus"
+
+		Activity activity = new Activity("io.appium.android.apis",
+				"io.appium.android.apis.preference.PreferenceDependencies");
+		driver.startActivity(activity);
+
+		//driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		//driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
+		
+		
 		driver.findElement(AppiumBy.id("android:id/checkbox")).click();
-		DeviceRotation landScape=new DeviceRotation(0,0,90);
+		DeviceRotation landScape = new DeviceRotation(0, 0, 90);
 		driver.rotate(landScape);
 		driver.findElement(AppiumBy.xpath("//android.widget.LinearLayout[2]")).click();
 		// driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.RelativeLayout")).click();
 		String alertTitle = driver.findElement(AppiumBy.id("android:id/alertTitle")).getText();
 		Assert.assertEquals(alertTitle, "WiFi settings");
-		driver.findElement(AppiumBy.id("android:id/edit")).sendKeys("Noushin");
+		// copy paste
+		// copy to clipboard -paste it to clipboard
+		driver.setClipboardText("Noushin");
+
+		String text = driver.getClipboardText();
+
+		driver.findElement(AppiumBy.id("android:id/edit")).sendKeys(text);
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+		driver.pressKey(new KeyEvent(AndroidKey.HOME));
 	}
 }
-
-
-
