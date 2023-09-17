e473fa69867d0c61a0314f933f1b451d72b7cf6b
Noushin <nou.babakhani@gmail.com>
2023-09-17 22:56:40 +0100

    Added Selenium support library and use it for test case number 2

--- src/test/java/noushinTest/eCommerce_tc_2.java
+++ src/test/java/noushinTest/eCommerce_tc_2.java
@@ -1,10 +1,13 @@
 package noushinTest;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -36,9 +39,16 @@ public class eCommerce_tc_2 extends BaseTest {
 				driver.findElements(AppiumBy.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
 			}
 		}
-
+ Thread.sleep(2000);
 		driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
-		Thread.sleep(3000);
+		
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.attributeContains(driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+		
+		String lastPageProduct=driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/productName")).getText();
+		Assert.assertEquals(lastPageProduct, "Jordan 6 Rings");
+		
+		
 
 	}
 
